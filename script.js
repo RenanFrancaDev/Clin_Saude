aaa99e4c39d93afc1c78420a6097236dcfb75200
Renan Franca <renanfranca.dev@gmail.com>
2022-10-26 11:52:16 -0300

    Acrescentado o resultado da classificacao na calculadora

--- script.js
+++ script.js
@@ -9,8 +9,36 @@ function calcular(){
     let valorPedo = inputPeso.value;
     
     let imc = valorPedo/(valorAltura * valorAltura);
+    let classificacao = "";
 
     resultado.innerHTML = imc.toFixed(1);
+
+    classificacao = classificaIMC();
+
+    classificacao_result.innerHTML = " Classificação: " + classificacao;
+
+    console.log(classificacao);
+    
+
+    function classificaIMC(){
+        
+        console.log(imc)
+
+        if(imc <= 18.5){
+            return "Magreza"
+        } else if(imc <= 24.9){
+            return "Normal"
+        } else if(imc <= 29.9){
+            return "Sobrepeso"
+        } else if(imc <= 39.9){
+                return "Obesidade"
+        } else{
+            return "Obesidade Grave"
+        }
+    }
+
 }
 
-console.log(imc)
+
+
+
